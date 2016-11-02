5e381cd3d29d2795a91eddf450e238754ba98dd8
hardmaru <hardmaru@gmail.com>
2016-11-02 16:31:45 -0700

    Update fw_ss.py

--- fw_ss.py
+++ fw_ss.py
@@ -13,38 +13,24 @@ ELEM_NUM = 26 + 10 + 1
 
 class FastWeightsRecurrentNeuralNetworks(object):
     def __init__(self, step_num, elem_num, hidden_num):
-        self.x = tf.placeholder(tf.float32, [None, step_num, elem_num])
-        self.y = tf.placeholder(tf.float32, [None, elem_num])
+        self.x = tf.placeholder(tf.float32, [100, step_num, elem_num])
+        self.y = tf.placeholder(tf.float32, [100, elem_num])
         self.l = tf.placeholder(tf.float32, [])
         self.e = tf.placeholder(tf.float32, [])
-
-        self.w1 = tf.Variable(tf.random_uniform([elem_num, 50], -np.sqrt(0.02), np.sqrt(0.02)), dtype=tf.float32)
-        self.b1 = tf.Variable(tf.zeros([1, 50]), dtype=tf.float32)
-        self.w2 = tf.Variable(tf.random_uniform([50, 100], -np.sqrt(0.01), np.sqrt(0.01)), dtype=tf.float32)
-        self.b2 = tf.Variable(tf.zeros([1, 100]), dtype=tf.float32)
         self.w3 = tf.Variable(tf.random_uniform([hidden_num, 100], -np.sqrt(0.01), np.sqrt(0.01)), dtype=tf.float32)
         self.b3 = tf.Variable(tf.zeros([1, 100]), dtype=tf.float32)
         self.w4 = tf.Variable(tf.random_uniform([100, elem_num], -np.sqrt(1.0/elem_num), np.sqrt(1.0/elem_num)), dtype=tf.float32)
         self.b4 = tf.Variable(tf.zeros([1, elem_num]), dtype=tf.float32)
 
-        self.w = tf.Variable(initial_value = 0.05 * np.identity(hidden_num), dtype=tf.float32)
-        self.c = tf.Variable(tf.random_uniform([100, hidden_num], -np.sqrt(hidden_num), np.sqrt(hidden_num)), dtype=tf.float32)
-        self.g = tf.Variable(tf.ones([1, hidden_num]), dtype=tf.float32)
-        self.b = tf.Variable(tf.zeros([1, hidden_num]), dtype=tf.float32)
-
         batch_size = tf.shape(self.x)[0]
 
-        a = tf.zeros(tf.pack([batch_size, hidden_num, hidden_num]), dtype=tf.float32)
-        h = tf.zeros([batch_size, hidden_num], dtype=tf.float32)
-
         cell = FastRNNCell(hidden_num)
+        state = cell.zero_state(batch_size=batch_size, dtype=tf.float32)
 
         for t in range(0, step_num):
-            z = tf.nn.relu(tf.nn.relu(tf.matmul(
-                tf.nn.relu(tf.matmul(self.x[:, t, :], self.w1) + self.b1),
-                self.w2) + self.b2
-            ))
-            _, (h, a) = cell(z, tf.nn.rnn_cell.LSTMStateTuple(h, a))
+            if t > 0:
+                tf.get_variable_scope().reuse_variables()
+            h, state = cell(self.x[:, t, :], state)
 
         h = tf.nn.relu(tf.matmul(h, self.w3) + self.b3)
         logits = tf.matmul(h, self.w4) + self.b4
@@ -65,9 +51,10 @@ class FastWeightsRecurrentNeuralNetworks(object):
                 bx, by = ar_data.train.next_batch(batch_size=batch_size)
                 loss, acc, _ = self.sess.run([self.loss, self.acc, self.trainer],
                                         feed_dict={self.x: bx, self.y: by, self.l: 0.9, self.e: 0.5})
-                print('Epoch: [{:4d}] [{:4d}/{:4d}] time: {:.4f}, loss: {:.8f}, acc: {:.2f}'.format(
-                    epoch, idx, batch_idxs, time.time() - start_time, loss, acc
-                ))
+                if (idx % 100 == 0):
+                    print('Epoch: [{:4d}] [{:4d}/{:4d}] time: {:.2f}, loss: {:.4f}, acc: {:.4f}'.format(
+                        epoch, idx, batch_idxs, time.time() - start_time, loss, acc
+                    ))
             if save > 0 and (epoch+1) % save == 0:
                 saver.save(self.sess, 'log/model', global_step=epoch)
 
